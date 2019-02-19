91d49ef802332ad150000a92b8ae33a0009054a6
Cristian Felipe <cristianpipe0494@gmail.com>
2019-02-19 15:49:37 -0600

    Have fixed tab's functionality. Still need to organize js code.

--- src/js/script.js
+++ src/js/script.js
@@ -4,20 +4,24 @@
   for (const elements of shippingContainer) {
 		const tabsElements = elements.querySelectorAll('ul > li > a');
 		const hiddenContent = elements.querySelectorAll('div .tab-content-hidden');
+		const showingContent = elements.querySelectorAll('div .tab-content-showing');
 
-		for (const links of tabsElements) {
-			links.addEventListener('click', function getHref (event) {
-				clearClass ();
-				const elementHref = event.target.getAttribute('href');
-				const tabsContent = elements.querySelector(elementHref);
-				tabsContent.setAttribute('class', 'tab-content-showing');
-			});
+		function clearClass() {
+			for (const content of hiddenContent) {
+				content.className = 'tab-content-hidden';
+			}
+			for (const content of showingContent) {
+				content.className = 'tab-content-hidden';
+			}
 		}
 
-		function clearClass () {
-			for (const content of hiddenContent) {
-				content.setAttribute('class', 'tab-content-hidden');
-			}
+		for (const links of tabsElements) {
+			links.addEventListener('click', function getHref(event) {
+				clearClass();
+				const elementHref = event.target.getAttribute('href');
+				const tabsContent = elements.querySelector(elementHref);
+				tabsContent.className = 'tab-content-showing';
+			});
 		}
 	}
 }());
