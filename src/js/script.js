1517640ec7d447689d3e2921b4051f6b5cba7af1
Cristian Felipe <cristianpipe0494@gmail.com>
2019-02-20 08:58:32 -0600

    Have fixed ESLinter errors on javascript's code, also created an IIFE function.

--- src/js/script.js
+++ src/js/script.js
@@ -1,34 +1,38 @@
-const shippingContainer = document.querySelectorAll('.ui-shipping-container');
+(function tabsProgram() {
+  const shippingContainer = document.querySelectorAll('.ui-shipping-container');
+  shippingContainer.forEach((shippingElements) => {
+    const elementsContainer = shippingElements;
+    const tabsElements = shippingElements.querySelectorAll('ul > li > a');
+    const hiddenContent = shippingElements.querySelectorAll('div .tab-content-hidden');
+    const showingContent = shippingElements.querySelectorAll('div .tab-content-showing');
 
-shippingContainer.forEach((shippingElements) => {
-  const elementsContainer = shippingElements;
-  const tabsElements = shippingElements.querySelectorAll('ul > li > a');
-  const hiddenContent = shippingElements.querySelectorAll('div .tab-content-hidden');
-  const showingContent = shippingElements.querySelectorAll('div .tab-content-showing');
+    function clearClass() {
+      hiddenContent.forEach((hiddenElement) => {
+        const cleanHideElement = hiddenElement;
+        cleanHideElement.className = 'tab-content-hidden';
+      });
+      showingContent.forEach((showingElement) => {
+        const cleanShowingElement = showingElement;
+        cleanShowingElement.className = 'tab-content-hidden';
+      });
+    }
 
-  function clearClass() {
-    hiddenContent.forEach((hiddenElement) => {
-      const cleanHideElement = hiddenElement;
-      cleanHideElement.className = 'tab-content-hidden';
-    });
-    showingContent.forEach((showingElement) => {
-      const cleanShowingElement = showingElement;
-      cleanShowingElement.className = 'tab-content-hidden';
-    });
-  }
+    function setDefault(tabsToDisplay) {
+      const defaultContent = tabsToDisplay;
+      defaultContent[0].className = 'tab-content-showing';
+    }
+    setDefault(hiddenContent);
 
-  function setDefault(tabsToDisplay) {
-    const defaultContent = tabsToDisplay;
-    defaultContent[0].className = 'tab-content-showing';
-  }
-  setDefault(hiddenContent);
-
-  tabsElements.forEach((element) => {
-    element.addEventListener('click', function (event) {
+    function getHref(event) {
       clearClass();
-      const elementHref = event.target.getAttribute('href');
+      const clickedElement = event;
+      const elementHref = clickedElement.target.getAttribute('href');
       const tabsContent = elementsContainer.querySelector(elementHref);
       tabsContent.className = 'tab-content-showing';
+    }
+
+    tabsElements.forEach((element) => {
+      element.addEventListener('click', getHref);
     });
   });
-});
+}());
