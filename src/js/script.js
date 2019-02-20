51f4e7b20a9afe2f55f89b3816ba15dd140c8f5f
Cristian Felipe <cristianpipe0494@gmail.com>
2019-02-19 18:43:06 -0600

    Have fixed functionality for tabs and accordion.

--- src/js/script.js
+++ src/js/script.js
@@ -1,27 +1,30 @@
-(function loadTab() {
-	const shippingContainer = document.querySelectorAll('.ui-shipping-container');
+const shippingContainer = document.querySelectorAll('.ui-shipping-container');
 
-  for (const elements of shippingContainer) {
-		const tabsElements = elements.querySelectorAll('ul > li > a');
-		const hiddenContent = elements.querySelectorAll('div .tab-content-hidden');
-		const showingContent = elements.querySelectorAll('div .tab-content-showing');
+for (const elements of shippingContainer) {
+	const tabsElements = elements.querySelectorAll('ul > li > a');
+	const hiddenContent = elements.querySelectorAll('div .tab-content-hidden');
+	const showingContent = elements.querySelectorAll('div .tab-content-showing');
 
-		function clearClass() {
-			for (const content of hiddenContent) {
-				content.className = 'tab-content-hidden';
-			}
-			for (const content of showingContent) {
-				content.className = 'tab-content-hidden';
-			}
+	function clearClass() {
+		for (const content of hiddenContent) {
+			content.className = 'tab-content-hidden';
 		}
-
-		for (const links of tabsElements) {
-			links.addEventListener('click', function getHref(event) {
-				clearClass();
-				const elementHref = event.target.getAttribute('href');
-				const tabsContent = elements.querySelector(elementHref);
-				tabsContent.className = 'tab-content-showing';
-			});
+		for (const content of showingContent) {
+			content.className = 'tab-content-hidden';
 		}
 	}
-}());
+
+	function setDefault (tabsToDisplay) {
+		tabsToDisplay[0].className = 'tab-content-showing';
+	}
+	setDefault(hiddenContent);
+
+	for (const links of tabsElements) {
+		links.addEventListener('click', function getHref(event) {
+			clearClass();
+			const elementHref = event.target.getAttribute('href');
+			const tabsContent = elements.querySelector(elementHref);
+			tabsContent.className = 'tab-content-showing';
+		});
+	}
+}
